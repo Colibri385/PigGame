36345ebfcee1c962f5d2e5e518429ffeca413dff
Colibri385 <colibri385@orange.fr>
2019-11-15 17:01:33 +0100

    erreur dice-2

--- app.js
+++ app.js
@@ -29,68 +29,68 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         if (gamePlaying) {
 
                 // 1. tirage nombre aléatoire
-                var dice = Math.floor(Math.random() * 6) + 1;
+                var dice1 = Math.floor(Math.random() * 6) + 1;
+                var dice2 = Math.floor(Math.random() * 6) + 1;
 
                 // 2. afficher le résultat
-                var diceDOM = document.querySelector('.dice');
-                diceDOM.style.display = 'block';
-                diceDOM.src = 'dice-' + dice + '.png';
+                document.getElementById('dice-1').style.display = 'block';
+                document.getElementById('dice-2').style.display = 'block';
+                document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+                document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
                 // 3. affiche le score sauf si le nombre est égal à 1
-                if (dice !== 1) {
+                if (dice1 !== 1 && dice2 !== 1) {
                         // Additionne le score
                         roundScore += dice;
- xtContent = roundScore;
- 
- 
- 
- 
- 
- 
+                        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+                } else {
+                        nextPlayer();
+                }
+
+        }
 });
 
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
-        
-        if(gamePlaying) {
 
-        // Mettre le score du tirage dans le Total général général
-        scores[activePlayer] += roundScore;
+        if (gamePlaying) {
 
-        // mise à jour
+                // Mettre le score du tirage dans le Total général général
+                scores[activePlayer] += roundScore;
 
-        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+                // mise à jour
 
-        // recupère le score final souhaité
-        var input = document.querySelector('.final-score').value;
-        var winningScore;
+                document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        // gère les entrée non définie , 0 , null , " " => false; sinon => true
+                // recupère le score final souhaité
+                var input = document.querySelector('.final-score').value;
+                var winningScore;
 
-        if (input) {
-                winningScore = input;
-        } else {
-                winningScore = 100;
+                // gère les entrée non définie , 0 , null , " " => false; sinon => true
+
+                if (input) {
+                        winningScore = input;
+                } else {
+                        winningScore = 100;
+                }
+
+
+                // vérifie si le joueur a gagné la partie 
+
+                if (scores[activePlayer] >= winningScore) {
+
+                        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+                        document.getElementById('dice-1').style.display = 'none';
+                        document.getElementById('dice-2').style.display = 'none';
+                        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+                        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+                        gamePlaying = false;
+                } else {
+                        // joueur suivant
+                        nextPlayer();
+                }
         }
-
-
-        // vérifie si le joueur a gagné la partie 
-
-        if (scores[activePlayer] >= winningScore) {
-
-                document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-                document.querySelector('.dice').style.display = 'none';
-                document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-                document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-                gamePlaying =false;
-        } else {
-                // joueur suivant
-                nextPlayer();
-        };
-
- }
-        
-}); 
+});
 
 
 function nextPlayer() {
@@ -112,9 +112,9 @@ function nextPlayer() {
         // document.querySelector('.player-0-panel').classList.remove('active');
         // Sdocument.querySelector('.player-1-panel').classList.add('active');
 
-        document.querySelector('.dice').style.display = 'none';
-
-};
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
+}
 
 // initialise le jeu
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -126,7 +126,8 @@ function init() {
         // gestion de fin de partie  - début partie variale true
         gamePlaying = true;
 
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
 
         document.getElementById('score-0').textContent = '0';
         document.getElementById('score-1').textContent = '0';
